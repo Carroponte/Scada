5972a8d9ea924918217ea1ffc3afee0da935a534
MARCHISIO <l.marchisio.2268@vallauri.edu>
2023-12-13 08:39:29 +0100

    init test connessione plc using c#

---
+++ TestConnessionePLC/TestConnessionePLC/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using S7.Net;
+
+using var plc = new Plc(CpuType.S7200, "192.168.2.111", 0, 0);
+
+try
+{
+
+    plc.Open();
+
+    if(!plc.IsConnected)
+    {
+        throw new Exception("Failed connection to plc");
+    }
+
+    
+
+}
+catch(Exception e)
+{
+    Console.Error.WriteLine(e);
+}
